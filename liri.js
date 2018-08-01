466b88241e25c734ecb4ed7a17fae9c1fb271f6a
SanSun <sandy.sundram@gmail.com>
2018-08-01 12:27:40 -0700

    added twitter api

--- liri.js
+++ liri.js
@@ -2,24 +2,35 @@ require("dotenv").config();
 var appKeys = require("./keys.js");
 var request = require("request");
 var Spotify = require('node-spotify-api');
+var Twitter = require('twitter');
 var fs = require("fs");
 
 var spotify = new Spotify(appKeys.spotify);
-// var client = new Twitter(appKeys.twitter);
+var client = new Twitter(appKeys.twitter);
 var app = process.argv[2];
 var songName;
 var movieName;
+var output;
+var input;
+var loopFlag = 0;
 
-// console.log(spotify);
 
+var dt = new Date();
+var utcDate = dt.toUTCString();
+
+writeToLog('-------------------------------------------NEW CALL-------------------------------------------')
+input = 'User input at command: '+process.argv.join(" ");
+writeToLog('\n'+input);
 
 if ((app != 'my-tweets') && (app != 'spotify-this-song') && (app != 'movie-this') && (app != 'do-what-it-says')){
     console.log("command not found: Please enter a valid command");
-}else if(app == 'my-tweets'){
+    writeToLog('\n'+"command not found: Please enter a valid command");
 
+}else if(app == 'my-tweets'){
+    twitterAPI();
 }else if(app == 'spotify-this-song'){
 
-spotifyAPI(process.argv[3]);
+    spotifyAPI(process.argv[3]);
 
 }else if (app == 'movie-this'){
 
@@ -28,56 +39,98 @@ spotifyAPI(process.argv[3]);
 }else if (app == 'do-what-it-says'){
     fs.readFile("random.txt","utf8",function(err,data){
         if (err) {
-            return console.log(err);
+            writeToLog('\n'+'Error occurred: ' + err);
+            return console.log('Error occurred: ' + err);
         }
-        // console.log(data);
         app = (data.split(','));
         if(app[0] == 'spotify-this-song'){
-            spotifyAPI(app[1]);
+            input = 'Read from random.txt: '+app[0]+' '+app[1];
+            writeToLog('\n'+input);
+            spotifyAPI(app[1]); 
         }else if(app[0] == 'movie-this'){
-            omdbAPI(app[1]);
+            input = 'Read from random.txt: '+app[0]+' '+app[1];
+            writeToLog('\n'+input);
+            omdbAPI(app[1]);    
         }
     });
+}
+
+function twitterAPI(){
+    var params = {screen_name: 'nodejs'};
+    client.get('statuses/user_timeline', params, function(error, tweets, response) {
+        if (!error) {
+            console.log("Last 20 tweets --");
+            writeToLog('\n'+"Output:"+'\n'+"Last 20 tweets --");
+            for (var i = 0; i<20; i++){
+                loopFlag++;
+                console.log('"'+tweets[i].text+"'"+'... created @ '+tweets[i].created_at);
+                console.log("---------------");
+                writeToLog('"'+tweets[i].text+"'"+'... created @ '+tweets[i].created_at+'\n'+"---------------");
+            }  
+        }
+    });
+    loopFlag = 0;
 }
 
 function spotifyAPI(song){
     songName = song;
     if(songName == null){
+        loopFlag++;
         console.log("\n"+"Since you did not enter a song name, we selected one for you...");
+        writeToLog('\n'+'Since you did not enter a song name, we selected one for you...');
         songName = "The Sign (US Album) [Remastered]";
     }
     spotify.search({ type: 'track', query: songName }, function(err, data) {
         if (err) {
+            writeToLog('\n'+'Error occurred: ' + err);
+            loopFlag = 0;
             return console.log('Error occurred: ' + err);
         }
-        // console.log(data.tracks);
-        console.log('\n');
-        console.log('Artist(s): '+data.tracks.items[0].album.artists[0].name); 
-        console.log('Song Name: '+data.tracks.items[0].name); 
-        console.log('Preview Link: '+data.tracks.items[0].external_urls.spotify); 
-        console.log('Album: '+data.tracks.items[0].album.name); 
+        output = '\n'+'Artist(s): '+data.tracks.items[0].album.artists[0].name+'\n'+
+                'Song Name: '+data.tracks.items[0].name+'\n'+
+                'Preview Link: '+data.tracks.items[0].external_urls.spotify+'\n'+
+                'Album: '+data.tracks.items[0].album.name;
+        console.log(output);
+        writeToLog('\n'+'Ouput: '+output);
     });
+    loopFlag = 0;
 }
 
 function omdbAPI(movie){
     movieName = movie;
     if(movieName == null){
-        console.log("\n"+" Since you did not enter a movie name, we selected one for you..."+"\n");
+        console.log("\n"+"Since you did not enter a movie name, we selected one for you..."+"\n");
         movieName = 'Mr. Nobody.';
+        writeToLog('\n'+"Since you did not enter a movie name, we selected one for you...");
     }
     var queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=trilogy";
     request(queryUrl,function(error,response,body){
         if(!error && response.statusCode === 200){
-            console.log('\n'+" Movie Title: "+JSON.parse(body).Title+'\n',
-                        "Year: "+JSON.parse(body).Year+'\n',
-                        "Rating: "+JSON.parse(body).Rated+'\n',
-                        "Rotten Tomatoes Rating: "+JSON.parse(body).Ratings[1].Value+'\n',
-                        "Country Produced: "+JSON.parse(body).Country+'\n',
-                        "Language: "+JSON.parse(body).Language+'\n',
-                        "Plot: "+JSON.parse(body).Plot+'\n',
-                        "Actors: "+JSON.parse(body).Actors+'\n');
+            output = '\n'+"Movie Title: "+JSON.parse(body).Title+'\n'+
+            "Year: "+JSON.parse(body).Year+'\n'+
+            "Rating: "+JSON.parse(body).Rated+'\n'+
+            "Rotten Tomatoes Rating: "+JSON.parse(body).Ratings[1].Value+'\n'+
+            "Country Produced: "+JSON.parse(body).Country+'\n'+
+            "Language: "+JSON.parse(body).Language+'\n'+
+            "Plot: "+JSON.parse(body).Plot+'\n'+
+            "Actors: "+JSON.parse(body).Actors+'\n';
+
+            console.log(output);
+            writeToLog('\n'+'Output: '+output);
         }else{
-            Console.log("Something didn't go quite right. Please try again...")
+            console.log("Something didn't go quite right. Please try again...");
+            writeToLog('\n'+"Something didn't go quite right. Please try again...");
         }
+    });
+}
+
+function writeToLog(logs){
+    if(loopFlag > 0){
+        addToFile = logs+'\n';
+    }else{
+        addToFile = dt.toUTCString()+logs+'\n';
+    }
+    fs.appendFile("./log.txt",addToFile,function(){
+
     })
 }
